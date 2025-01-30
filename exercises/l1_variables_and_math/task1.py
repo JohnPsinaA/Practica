6499fddd170ff5ec62675b48555e37cfbb46014a
JohnPsinaA <154378446+JohnPsinaA@users.noreply.github.com>
2025-01-30 10:55:05 +0000

    1-12tasks

--- exercises/l1_variables_and_math/task1.py
+++ exercises/l1_variables_and_math/task1.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     a, b = 10, 5
 
      
-    c = None
+    c = a+b
 
     # Do not change the line below
     assert c == 15
--- exercises/l1_variables_and_math/task4.py
+++ exercises/l1_variables_and_math/task4.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
 
     # Assign the values of correct types to variables a, b, c, d 
     # to make the script work without errors
-
+    a = 1
+    b = 1.1
+    c = 1 + 1j
+    d = "aboba"
+    print(a, b, c, d)
     # Do not change the lines below
     assert isinstance(a, int)
     assert isinstance(b, float)
